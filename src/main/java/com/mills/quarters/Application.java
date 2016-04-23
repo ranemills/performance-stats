cc37f4afba6198d4fdd17f304427df799e169072
Ryan Mills <rem518@york.ac.uk>
2016-04-23 09:47:38 +0100

    Make Mongo work on Openshift

--- src/main/java/com/mills/quarters/Application.java
+++ src/main/java/com/mills/quarters/Application.java
@@ -1,6 +1,7 @@
 package com.mills.quarters;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,7 +9,10 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -47,9 +51,24 @@ public class Application extends SpringBootServletInitializer {
         return new Mongo("localhost");
     }
 
-    public
     @Bean
-    MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "quarter");
+    public MongoTemplate mongoTemplate() throws Exception {
+
+        if (System.getenv("OPENSHIFT_MONGODB_DB_HOST") != null) {
+            String openshiftMongoDbHost = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+            int openshiftMongoDbPort = Integer.parseInt(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
+            String username = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+            String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+            Mongo mongo = new MongoClient(openshiftMongoDbHost, openshiftMongoDbPort);
+            UserCredentials userCredentials = new UserCredentials(username, password);
+            String databaseName = System.getenv("OPENSHIFT_APP_NAME");
+            MongoDbFactory mongoDbFactory = new SimpleMongoDbFactory(mongo, databaseName, userCredentials);
+            MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory);
+            return mongoTemplate;
+
+        } else {
+            String databaseName = "performances";
+            return new MongoTemplate(new SimpleMongoDbFactory(new MongoClient(), databaseName));
+        }
     }
 }
